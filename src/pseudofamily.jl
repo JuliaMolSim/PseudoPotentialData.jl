23aec1c486c5aaf23f1d4b928ba498d6b5cd8780
Michael F. Herbst <info@michael-herbst.com>
2024-11-19 07:48:27 +0100

    Remove incompatible entries from artifact

--- src/pseudofamily.jl
+++ src/pseudofamily.jl
@@ -13,7 +13,10 @@ end
 
 """
 Construction of a PseudoFamily from a `identifier` representing
-the pseudopotential family to use.
+the pseudopotential family to use. For a list of valid identifier,
+see [`family_identifiers`](@ref).
+A `PseudoFamily` is an `AbstractDict{Symbol,String} mapping from
+an element symbol to the full path of the pseudopotential file.
 """
 function PseudoFamily(identifier::AbstractString)
     artifact_file = find_artifacts_toml(@__FILE__)
